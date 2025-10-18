f35eca51c48b1df6b11b09a044c3b9ba19e79ffe
Usama Rafaqat <chusama637@gmail.com>
2025-10-19 03:53:03 +0500

    Fix import path for useTodo in TodoItem component

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 // import { useTodo } from './Context';
-import { useTodo } from '../context'
+import { useTodo } from '../Context'
 
 function TodoItem({ todo }) {
     const [isTodoEditable, setIsTodoEditable]= useState(false)
